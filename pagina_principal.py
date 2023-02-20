1f25f6a9a8cf7c805bfb2560299b176a1582977d
Jose <al_jperaltaa@iessierradelasvillas.org>
2023-02-20 11:01:10 +0100

    PROGRAMA TERMINADO

--- pagina_principal.py
+++ pagina_principal.py
@@ -6,16 +6,15 @@ def main(page: ft.Page):
     page.bgcolor = ft.colors.GREEN_200
     page.window_height = 550
     page.window_width = 800
-    page.on_resize = False
+    page.window_resizable = False
     page.horizontal_alignment=ft.CrossAxisAlignment.CENTER
-
+    cont=ft.TextField(value=1)
     page.update()
 
 
     img = ft.Image(src=f"/IMAGENES/inicio_de_sesion.png",
     width=225,
     height=200)
-
     tb1 = ft.TextField(label="Usuario",width=400)
     tb2 = ft.TextField(label="Contraseña", password=True, can_reveal_password=True,width=400)
     colDatos = ft.Column(controls=[tb1,tb2],spacing=20)
@@ -24,34 +23,39 @@ def main(page: ft.Page):
     def comprobar_login(e):
         vDatos=[]
         f = open("usu.txt","r")
-        contador=3
+        contador=cont.value
+        
         for linea in f:
             l = linea.replace("\n","")
             vDatos.append(l)
-
-        if (vDatos.count(tb1.value)>0) and (vDatos.count(tb2.value)>0):
+        
+        if  (vDatos.count(tb1.value)>0) and (vDatos.count(tb2.value)>0):
             dlg = ft.AlertDialog(title=ft.Text("Usuario correcto"))
             page.dialog = dlg
             dlg.open = True
             page.update()
 
         else:
-            contador=(contador-1)
-            dlg = ft.AlertDialog(title=ft.Text(f"Te quedan {contador} intentos"))
-            page.dialog = dlg
-            dlg.open = True
-            page.update()
-            if contador<3:
-                contador=(contador-1)
-                dlg = ft.AlertDialog(title=ft.Text(f"Te quedan {contador} intentos"))
+            if contador==1:
+                dlg = ft.AlertDialog(title=ft.Text(f"Usuario incorrecto"))
                 page.dialog = dlg
                 dlg.open = True
+                contador=contador+1
                 page.update()
-                if contador==3:
-                    dlg = ft.AlertDialog(title=ft.Text("Usuario incorrecto"))
-                    page.dialog = dlg
-                    dlg.open = True
-                    page.update()
+            elif contador==2:
+                dlg2 = ft.AlertDialog(title=ft.Text(f"Te queda un intento"))
+                page.dialog = dlg2
+                dlg2.open = True
+                contador=contador+1
+                page.update()
+            elif contador==3:
+                dlg3 = ft.AlertDialog(title=ft.Text(f"Usuario bloqueado"))
+                page.dialog = dlg3
+                dlg3.open = True
+                contador=contador+1
+                page.update()
+            cont.value=contador
+        page.update()
 
     boton=ft.ElevatedButton("INICIAR SESION", icon="settings",bgcolor=ft.colors.GREEN_100,on_click=comprobar_login)
 
